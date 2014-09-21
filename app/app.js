0f173cf6ec3812a859d78fa2bc86e16964b21cb3
Martin Becker <martinbecker.de@gmail.com>
2014-09-21 12:40:51 +0200

    added impressum

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ aszelApp.config(function ($routeProvider) {
             controller: 'AszelController',
             templateUrl: '/app/partials/clowns.html'
         })
+        .when('/disclaimer', {
+            controller: 'AszelController',
+            templateUrl: '/app/partials/impressum.html'
+        })
         .otherwise({
             templateUrl: '/app/partials/404.html'
         });
