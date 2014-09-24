ce5fc6ff6ccc48a69a8d2edf7fe35af41e0ac4b4
Martin Becker <martinbecker.de@gmail.com>
2014-09-24 21:47:06 +0200

    added function to get the name of a weekday

--- app/factories/EventFactory.js
+++ app/factories/EventFactory.js
@@ -16,19 +16,29 @@ aszelApp.factory('EventFactory', function(){
         // events per day
         var eventsPerDay = [];
         var dayCounter = 0;
+
         var currentDate = getCurrentDate();
-        console.log(year +''+ month +''+ day);
 
         $http.get(url)
         .success(function(data, status, headers, config) {
 
-            console.log(data);
+            // console.log(data);
+            var lastDate = "";
 
+            // loop over all events from spreadsheet
             for (var i = 0; i < data.feed.entry.length; i++) {
+
+                // compare date of event with date of event before
+                var dateOfCurEvent = data.feed.entry[i]['gsx$date']['$t'];
+                var weekday = getWeekDayAsWord(dateOfCurEvent);
+
+                console.log("date of current event: " + dateOfCurEvent);
+
 
                 eventsPerDay[dayCounter] =
                 {
-                    date: data.feed.entry[i]['gsx$date']['$t'],
+                    weekday: weekday,
+                    date: dateOfCurEvent,
                     time: data.feed.entry[i]['gsx$time']['$t'],
                     place: data.feed.entry[i]['gsx$place']['$t'],
                     title: data.feed.entry[i]['gsx$title']['$t'],
@@ -50,15 +60,46 @@ aszelApp.factory('EventFactory', function(){
     };
 
     /**
-     * Function returns the current date in format 141212
+     * Function returns the current date in format 20141212
      * @return {String}
      */
     function getCurrentDate() {
         var d = new Date();
         var year = d.getFullYear();
-        var month = d.getMonth()+1;
-        var day = d.getDate();
-        return year +''+ month +''+ day;
+        var month = d.getMonth() + 1;
+        month = month < 10 ? '0' + month : month;
+        var day = d.getUTCDate();
+        day = day < 10 ? '0' + day : day;
+        var result = year +''+ month +''+ day;
+        console.log(result);
+        return result;
+    }
+
+    /**
+     * Function returns the name of the weekday
+     * @param  {String} datestring Format: 20141212
+     * @return {String}
+     */
+    function getWeekDayAsWord(datestring) {
+
+        // split datestring into year, month and day
+        // keep in mind that month is in range between 0 and 11
+        var year = datestring.substring(0, 4);
+        var month = datestring.substring(4, 6);
+        month = month-1;
+        var day = datestring.substring(6, 8);
+
+        var d = new Date(year, month, day);
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        console.log("Name of day " + datestring + " : " + weekday[d.getDay()]);
+        return weekday[d.getDay()];
     }
 
     return factory;
